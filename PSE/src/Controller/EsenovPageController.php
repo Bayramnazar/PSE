445907dbec71c77d3cdf839062e0cb3546da90a3
Bayramnazar Esenov <bayramnazaresenovv@gmail.com>
2026-04-26 09:04:16 +0300

    Add Esenov personal page controller and template

---
+++ PSE/src/Controller/EsenovPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EsenovPageController extends AbstractController
+{
+    #[Route('/esenov/page', name: 'app_esenov_page')]
+    public function index(): Response
+    {
+        return $this->render('esenov_page/index.html.twig', [
+            'controller_name' => 'EsenovPageController',
+        ]);
+    }
+}
